72f457a8fb9b9e42f350668cdd2d15b42f2ae0c5
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-09 20:17:36 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,81 @@
+def sort_array_asc(array)
+  array.sort
+end 
+
+ 
+def sort_array_desc(array)
+ array.sort_by {|x| -x}
+end 
+
+
+def sort_array_char_count(array)
+  array.sort_by {|x| x.length}
+end 
+
+
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+array
+end 
+
+  
+def reverse_array(array)
+    array.reverse
+end 
+
+
+def kesha_maker(array)
+  array.each.with_index {|word| word[2] = "$"}
+end 
+
+
+
+
+def find_a(array)
+    array.find_all {|word| word.start_with?("a")}
+end 
+
+
+
+def sum_array(array)
+  array.inject {|x,y| x + y}
+end 
+
+
+def add_s(array)
+  
+  array.collect.with_index {|word, index|
+    if index == 1
+     word
+    else
+     word + "s"
+    end 
+  }
+end 
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 
